4f68c288360777c85f1be28b666b411aa81e22cc
sato kazuki <dracrowa@gmail.com>
2016-12-11 19:50:30 +0900

    butterKnifeを導入

--- app/src/main/java/com/kaz/constraintproject/MainActivity.java
+++ app/src/main/java/com/kaz/constraintproject/MainActivity.java
@@ -1,13 +1,50 @@
 package com.kaz.constraintproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 
-public class MainActivity extends Activity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textViewMain) TextView textViewMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withTranslucentStatusBar(false)
+                .withActionBarDrawerToggle(false)
+                .addDrawerItems(
+                        //pass your items here
+                )
+                .build();
+
+    }
+
+    public void btnTapped(View view){
+        textViewMain.setText("test");
+        //Intent intent = new Intent(getApplication(), Main2Activity.class);
+        //startActivity(intent);
     }
 }
